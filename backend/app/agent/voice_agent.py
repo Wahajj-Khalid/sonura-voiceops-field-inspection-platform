656531fea8f616704a98d49939bf910a823d5670
Wahajj-Khalid <wahajjkhalid03@gmail.com>
2026-09-21 21:11:37 +0500

    changing python version for oom issues on voice agent

--- backend/app/agent/voice_agent.py
+++ backend/app/agent/voice_agent.py
@@ -250,7 +250,6 @@ if __name__ == "__main__":
     cli.run_app(
         WorkerOptions(
             entrypoint_fnc=entrypoint,
-            num_idle_processes=0,
             port=assigned_port
         )
     )
